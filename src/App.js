cd7a2cad8c3f948fc19e9a924b4e4873ee1c0681
asadhijj <hijjawi30@gmail.com>
2022-12-28 23:52:12 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Parent from "./components/Parent";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Parent />}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App
